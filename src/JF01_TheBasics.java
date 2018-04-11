83d25de145670ed6fe51cc8052eb8f2c644bdb91
Marcelo Barsotti <mbarsott@gmail.com>
2018-04-10 20:52:07 -0500

    Fixes minor issues on comments

--- src/JF01_TheBasics.java
+++ src/JF01_TheBasics.java
@@ -1,6 +1,6 @@
-// The Basics - Comments, classes, and entry points
+// Java Fundamentals 01 - The Basics - Comments, classes, and entry points
 
-// This is a comment line. Everything after the // is ignored by the Java compiler (until the end of the line).
+// This is a comment line. Everything after the // is ignored by the Java compiler (up to the end of the line).
 
 /*
     This is a comment block. Everything between "whack asterisk" and "asterisk whack"is ignored...
@@ -18,7 +18,7 @@
 // as the class plus the extension ".java".
 public class JF01_TheBasics {
 
-  // This a program entry point. If a method (function of a class) has this exact signature:
+  // This a program entry point. If a method (function inside a class) has this exact signature:
   // public static void main(String[] args)
   // it can be where your program starts running, the entry point.
   public static void main(String[] args) {
@@ -33,13 +33,12 @@ public class JF01_TheBasics {
   /*
     To compile and run this program:
     1. Download and install the Java JDK: http://www.oracle.com/technetwork/java/javase/downloads/index.html
-    recommendation: start out with the latest Java 8 JDK (Java 1.8)
-    2. From the command line, on the directory where this file is, execute the command: javac "JF01_TheBasics.java
+    recommendation: start out with the latest Java 8 or 9 JDK (Java 1.8 or 1.9)
+    2. From the command line, on the folder where this file is, execute the command: "javac JF01_TheBasics.java"
     Obs.: if you list the contents of the folder you should see the file JF01_TheBasics.class (the compiled class)
-    3. From the command line, execute the command "java JF01_TheBasics""
+    3. From the command line, on that same folder, execute the command "java JF01_TheBasics""
     Result: you should see the string "Hello Java Fundamentals!" on the screen.
   */
 }
 
-// Next Source code: JF02_Packages.java
-// This file will be under com/fledger/javafundamentals
+// Next Source code: src/com/fledger/javafundamentals/JF02_Packages.java -> https://goo.gl/DnMZgs
--- src/com/fledger/javafundamentals/JF02_Packages.java
+++ src/com/fledger/javafundamentals/JF02_Packages.java
@@ -1,31 +1,34 @@
-// Packages determine the folder structure of your projects.
+// Java Fundamentals 02 - Packages
+
+// Packages determine the folder structure of your projects. By convention they are all lowercase.
 // They are used to organize your source code and create scope to avoid name collisions.
 // If you use a package, it should be the first statement of your  source file.
-// To keep names unique, package names usually start with the domain name inverted
-// ex.: fledger.com -> com.fledger
-// after the domain name you determine the package hierarchy to organize your code
+// To keep names unique, package names usually start with the web domain name inverted
+// ex.: fledger.com -> "com.fledger"
+// following the inverted domain name, you name package hierarchy to organize your code
 // The folder structure should reflect the package structure, from the root of source code.
-// ex.: com.fledger.javafundamentals => src/com/fledger/javafundamentals (if src is the root of source code)
+// ex.: "com.fledger.javafundamentals" -> src/com/fledger/javafundamentals (if src is the root of source code)
 package com.fledger.javafundamentals;
 
 // Imports let you use code from other classes or packages
 
-// The following statement lets you use the class Date from the package java.util:
+// The following statement lets you use the class Date from the package "java.util":
 import java.util.Date;
 
-// The following statement lets you use any class from the java.text package (including DateFormat):
+// The following statement lets you use any class from the "java.text" package (including "DateFormat"):
 import java.text.*;
 
-// The fully qualified name of this class is com.fledger.javafundamentals.JF02_Packages
+// The fully qualified name of this class is "com.fledger.javafundamentals.JF02_Packages"
 public class JF02_Packages {
 
     // You already know what an entry point is:
     public static void main(String[] args) {
-      // Creates an object now from the class Date (contains date and time). It is initialized to the current time.
+      // This creates an object called "now" from the class "Date" (that contains date and time).
+      // It is initialized to the current time.
       Date now = new Date();
-      // Creates an object to format a Date with the long time format.
+      // This creates an object to format a "Date" with the long time format.
       DateFormat dateFormat = DateFormat.getTimeInstance(DateFormat.LONG);
-      // Writes the current time using the long time format.
+      // This writes the current time using the long time format.
       System.out.println("Now it is " + dateFormat.format(now));
     }
 
@@ -33,12 +36,15 @@ public class JF02_Packages {
       To compile and run this program:
       1. From the command line, on the source root folder, execute the command:
       javac com/fledger/javafundamentals/JF02_Packages.java
-      Obs.: it will create JF02_Packages.class in that folder (javafundamentals)
-      2. From the command line, execute the command
+      Obs.: it will create a "JF02_Packages.class" file in that folder (javafundamentals)
+      2. From the command line, execute the command (still from the source folder)
       java com.fledger.javafundamentals.JF02_Packages
-      Notice that you compile (javac) a file (with .java extension)
-      and run (java) a class (with fully qualified name)
-      Result: you should see "Now it is " and the current time, long format, on the screen.
-      By the way, it is about time for you to use an IDE. I recommend Eclipse or IntelliJ Idea Community!
+      Notice that you compile (javac) a file (with .java extension),
+      and you run (java) a class (using its fully qualified name)
+      Result: you should see "Now it is " followed by the current time on the screen.
+      By the way, it is about time for you to use an IDE. I recommend Eclipse or IntelliJ Idea CE, both free!
     */
 }
+
+// previous file: src/JF01_TheBasics.java -> https://goo.gl/rJN5Aj
+// next file: src/com/fledger/javafundamentals/JF03_xxx.java ->
